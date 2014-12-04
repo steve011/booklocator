40a0934e51493c2f5fae4c33fbfa382d8891cd6a
steve011 <stevenwasi@gmail.com>
2014-12-04 12:33:35 -0500

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
   <h2><a href="http://cise.ufl.edu/~sw5/Book_Locator/adminer.php?oracle=oracle.cise.ufl.edu%2Forcl&username=sw5&db=CISETS&ns=SW5">Database Manager (for testing purposes...)</a></h2>
   <?php
   $conn = oci_connect('hr', 'welcome', 'localhost/XE');
-  $stid = oci_parse($conn, SELECT * FROM books);
+  $stid = oci_parse($conn, 'SELECT * FROM books');
   oci_execute($stid);
   echo "<table border='1'>\n";
   while($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULL)){
