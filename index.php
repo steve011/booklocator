9a2ba1f9d70e905a74d4d808e6cd1ad905f35d7f
steve011 <stevenwasi@gmail.com>
2014-12-06 01:23:57 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,29 +2,127 @@
 <?php require ('../connect.php'); ?>
 
 
-<html><head><title>Home Page</title></head>
-<body>
-
-
-  <h2><a href="http://cise.ufl.edu/~sw5/Book_Locator/ui.php">Front End</a></h1>
-  <h2><a href="http://cise.ufl.edu/~sw5/Book_Locator/search.php">Search</a></h1> 
-  <h2><a href="http://cise.ufl.edu/~sw5/Book_Locator/admin.php">Back End</a></h1> 
-  <h2><a href="http://cise.ufl.edu/~sw5/Book_Locator/adminer.php?oracle=oracle.cise.ufl.edu%2Forcl&username=sw5&db=CISETS&ns=SW5">Database Manager (for testing purposes...)</a></h2>
-  <?php
-  /*Don't need this: $conn = oci_connect('hr', 'welcome', "localhost/XE", 'SW5'); 
-    Just use $connection instead; the variable is intialized in connect.php, which is included above.*/
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="bootstrap.css">
+		<link rel="stylesheet" href="font-awesome-4.2.0/css/font-awesome.css">
+		<link rel="stylesheet" href="font-awesome-4.2.0/css/font-awesome.min.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	</head>
+	<body>	
+		<header>
+			<div class="container" style="height:150px;">
+				<div class="row" style="">
+					<div class="col-xs-8 col-xs-push-2" style="height:100%;background-color:#1ad2ff;z-index:2;border: 3px solid black;border-radius:10px;margin-top:-5px;">
+						<img style="float:left;" src="book-logo.jpg" style="margin-top:10%;width:100%:height:auto;">
+						<h1 style="font-family:cursive;font-weight:bold;text-align:center;font-style:italic;margin-top:25px;letter-spacing:2px;color:white">BOOK LOCATOR</h1>
+					</div>
+				</div>
+				<Div class="row">
+					<div class="col-xs-6 col-xs-push-3 menu-bar" style="background-color:#ff471a;z-index:1;">
+							<ul>
+								<li><a href="index.html">Home</a></li>
+								<li><a href="collections.html">Store<a></li>
+								<li><a href="#">Search</a></li>
+								<li><a href="#">Support</a></li>
+							</ul>
+					</div>
+				</div>
+			</div>
+		</header>
+		<div class="container">
+			<img src="library.png" style="width:100%;height:auto;">
+		</div>
+		<div class="container" style="margin-top:20px;">
+			<div class="row" style="background-color:#ffcc00;">
+					<h1 style="text-align:center;">New Arrivals</h1>
+					<div class="col-xs-2" style="height:300px;background-color:#ff004d;margin:19.5px;">
+						<div style="height:250px;width:100%;"></div>
+						<div style="height:50px;width:100%;text-align:center;color:white;border-top:1px solid black;">
+							<p>Product Name</p>
+							<p>{{ Price }}</p>
+						</div>
+					</div>
+					<div class="col-xs-2" style="height:300px;background-color:#ff004d;margin:19.5px;">
+						<div style="height:250px;width:100%;"></div>
+						<div style="height:50px;width:100%;text-align:center;color:white;border-top:1px solid black;">
+							<p>Product Name</p>
+							<p>{{ Price }}</p>
+						</div>
+					</div>
+					<div class="col-xs-2" style="height:300px;background-color:#ff004d;margin:19.5px;">
+						<div style="height:250px;width:100%;"></div>
+						<div style="height:50px;width:100%;text-align:center;color:white;border-top:1px solid black;">
+							<p>Product Name</p>
+							<p>{{ Price }}</p>
+						</div>
+					</div>
+					<div class="col-xs-2" style="height:300px;background-color:#ff004d;margin:19.5px;">
+						<div style="height:250px;width:100%;"></div>
+						<div style="height:50px;width:100%;text-align:center;color:white;border-top:1px solid black;">
+							<p>Product Name</p>
+							<p>{{ Price }}</p>
+						</div>
+					</div>
+					<div class="col-xs-2" style="height:300px;background-color:#ff004d;margin:19.5px;">
+						<div style="height:250px;width:100%;"></div>
+						<div style="height:50px;width:100%;text-align:center;color:white;border-top:1px solid black;">
+							<p>Product Name</p>
+							<p>{{ Price }}</p>
+						</div>
+					</div>
+			</div>	
+		</div>
+		<div class="container" style="margin-top:20px;">
+			<div class="row" style="background-color:#ff3300;">
+					<h1 style="text-align:center;">Categories</h1>
+					<div class="col-xs-2" style="height:300px;background-color:#00ccff;margin:19.5px;">
+						<div style="height:250px;width:100%;"></div>
+						<div style="height:50px;width:100%;text-align:center;color:white;border-top:1px solid black;">
+							<p>Product Name</p>
+							<p>{{ Price }}</p>
+						</div>
+					</div>
+					<div class="col-xs-2" style="height:300px;background-color:#00ccff;margin:19.5px;">
+						<div style="height:250px;width:100%;"></div>
+						<div style="height:50px;width:100%;text-align:center;color:white;border-top:1px solid black;">
+							<p>Product Name</p>
+							<p>{{ Price }}</p>
+						</div>
+					</div>
+					<div class="col-xs-2" style="height:300px;background-color:#00ccff;margin:19.5px;">
+						<div style="height:250px;width:100%;"></div>
+						<div style="height:50px;width:100%;text-align:center;color:white;border-top:1px solid black;">
+							<p>Product Name</p>
+							<p>{{ Price }}</p>
+						</div>
+					</div>
+					<div class="col-xs-2" style="height:300px;background-color:#00ccff;margin:19.5px;">
+						<div style="height:250px;width:100%;"></div>
+						<div style="height:50px;width:100%;text-align:center;color:white;border-top:1px solid black;">
+							<p>Product Name</p>
+							<p>{{ Price }}</p>
+						</div>
+					</div>
+					<div class="col-xs-2" style="height:300px;background-color:#00ccff;margin:19.5px;">
+						<div style="height:250px;width:100%;"></div>
+						<div style="height:50px;width:100%;text-align:center;color:white;border-top:1px solid black;">
+							<p>Product Name</p>
+							<p>{{ Price }}</p>
+						</div>
+					</div>
+			</div>	
+		</div>
+		<footer>
+			<div class="container-fluid;" style="background-color:black;height:200px;margin-top:20px;"></div>
+		</footer>
+	</body>
+</html>
   
   
-  $stid = oci_parse($connection, 'SELECT * FROM books WHERE ROWNUM <= 5 ORDER BY DBMS_RANDOM.RANDOM'); /* Added "WHERE ROWNUM <= 1000", takes forever to load otherwise */
-  oci_execute($stid);
-  while($row = oci_fetch_array($stid))
-  {
-    echo '<img src="'.htmlentities($row["IMAGE_URL_L"]).'">';
-  }
   
     
   
-  ?>
 <?php
 oci_close($connection);
 ?>
