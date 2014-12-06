d66a529b41df51302a7e3796aa385a2e4c29b262
steve011 <stevenwasi@gmail.com>
2014-12-06 01:53:52 -0500

    Update index.php

--- index.php
+++ index.php
@@ -40,8 +40,8 @@
   					{
   					echo '<div class="col-xs-2" style="height:300px;background-color:#ff004d;margin:19.5px;">';
 					echo '<img style="height:250px;width:100%;" src="'.htmlentities($row["IMAGE_URL_L"]).'">';
-					echo '<div class="height:50px;width:100%;text-align:center;color:white;border-top:1px solid black;">';
-					echo '<p style="font-size:12px">'.htmlentities($row["TITLE"]).'</p>';
+					echo '<div class="width:100%;text-align:center;color:white;border-top:1px solid black;">';
+					echo '<p style="font-size:12px;text-align:center;">'.htmlentities($row["TITLE"]).'</p>';
 					echo '</div></div>';
 					}
 					 ?>
