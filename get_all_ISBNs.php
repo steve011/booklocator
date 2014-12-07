e974612bcef318b50b2de3167748153f66bc9c5e
steve011 <stevenwasi@gmail.com>
2014-12-06 22:01:48 -0500

    Update get_all_ISBNs.php

--- get_all_ISBNs.php
+++ get_all_ISBNs.php
@@ -7,8 +7,16 @@ oci_execute($stid);
 
 $myfile = fopen("ISBNs.txt", "w") or die("Unable to open file!");
 
-while (($row = oci_fetch_array($stid, OCI_BOTH)) != false) {
-    fwrite($myfile, $row[0]."\n");
+while (($row = oci_fetch_array($stid)) != false) {
+	for($i = 0; $i < 10; $i++){
+		if($row){
+			fwrite($myfile, $row[0]." ");
+			$row = oci_fetch_array($stid);
+		}else{
+			break;
+		}
+	}
+	fwrite($myfile, "\n");
 }
 
 fclose($myfile);
