f863cf91dc4b032c2ce608688a600dafb709edad
steve011 <stevenwasi@gmail.com>
2014-12-06 22:32:35 -0500

    Update get_all_ISBNs.php

--- get_all_ISBNs.php
+++ get_all_ISBNs.php
@@ -2,24 +2,50 @@
 <?php require ('../connect.php'); ?> 
 
 <?php
-$stid = oci_parse($connection, 'SELECT ISBN FROM Books');
-oci_execute($stid);
+
 
 $myfile = fopen("ISBNs.txt", "w") or die("Unable to open file!");
 
-while (($row = oci_fetch_array($stid)) != false) {
-	for($i = 0; $i < 10; $i++){
-		if($row){
-			fwrite($myfile, $row[0]." ");
-			$row = oci_fetch_array($stid);
-		}else{
-			break;
+$offset = 0;
+$numrows = 25000;
+
+while($offset <= 231910){
+	$sql = 'SELECT ISBN FROM Books';
+
+	$sql = "SELECT * FROM (SELECT a.*, ROWNUM AS my_rnum
+						   FROM ($sql) a
+						   WHERE ROWNUM <= :offset + :numrows)
+			WHERE my_rnum > :offset";
+
+
+	$stid = oci_parse($connection, $sql);
+	oci_bind_by_name($stid, ':numrows', $numrows);
+	oci_bind_by_name($stid, ':offset', $offset);
+	oci_execute($stid);
+
+	while (($row = oci_fetch_array($stid, OCI_ASSOC + OCI_RETURN_NULLS)) != false) {
+		for($i = 0; $i < 10; $i++){
+			if($row){
+				fwrite($myfile, $row[0]." ");
+				$row = oci_fetch_array($stid);
+			}else{
+				break;
+			}
 		}
+		fwrite($myfile, "\n");
 	}
-	fwrite($myfile, "\n");
+	$offset += 25000;
+	$numrow += 25000;
+	echo "Completed ".$offset." rows.";
+	fclose($myfile);
+	$myfile = fopen("ISBNs.txt", "a") or die("Unable to open file!");
+	
+	echo "Waiting...";
+	sleep(15);
+	
 }
 
-fclose($myfile);
+//fclose($myfile);
 oci_free_statement($stid);
 ?>
 
