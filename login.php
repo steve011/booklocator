36fde08b14e6da9b37680b6d7e0fa3aa2f2a1cac
steve011 <stevenwasi@gmail.com>
2014-12-07 00:57:09 -0500

    Create login.php

---
+++ login.php
@@ -0,0 +1,67 @@
+#!/usr/local/bin/php
+<?php require ('../connect.php'); ?>
+
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="bootstrap.css">
+		<link rel="stylesheet" href="font-awesome-4.2.0/css/font-awesome.css">
+		<link rel="stylesheet" href="font-awesome-4.2.0/css/font-awesome.min.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	</head>
+	<body>	
+		<header>
+			<div class="container" style="height:150px;">
+				<div class="row" style="">
+					<div class="col-xs-8 col-xs-push-2" style="height:100%;background-color:#1ad2ff;z-index:2;border: 3px solid black;border-radius:10px;margin-top:-5px;">
+						<img style="float:left;" src="http://i.imgur.com/UX50fa2.jpg" style="margin-top:10%;width:100%:height:auto;">
+						<h1 style="font-family:cursive;font-weight:bold;text-align:center;font-style:italic;margin-top:25px;letter-spacing:2px;color:white">BOOK LOCATOR</h1>
+					</div>
+				</div>
+				<Div class="row">
+					<div class="col-xs-6 col-xs-push-3 menu-bar" style="background-color:#ff471a;z-index:1;">
+							<ul>
+								<li><a href="index.html">Home</a></li>
+								<li><a href="collections.html">Store<a></li>
+								<li><a href="http://cise.ufl.edu/~sw5/Book_Locator/admin.php">Search</a></li>
+								<li><a href="http://cise.ufl.edu/~sw5/Book_Locator/adminer.php?oracle=oracle.cise.ufl.edu%2Forcl&username=sw5&db=CISETS&ns=SW5">Support</a></li>
+							</ul>
+					</div>
+				</div>
+			</div>
+		</header>
+		<br></br>
+		<div class="container">
+		  <div class="row">
+		    <table width="300" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
+<tr>
+<form name="form1" method="post" action="checklogin.php">
+<td>
+<table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
+<tr>
+<td colspan="3"><strong>Member Login </strong></td>
+</tr>
+<tr>
+<td width="78">Username</td>
+<td width="6">:</td>
+<td width="294"><input name="myusername" type="text" id="myusername"></td>
+</tr>
+<tr>
+<td>Password</td>
+<td>:</td>
+<td><input name="mypassword" type="text" id="mypassword"></td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td>&nbsp;</td>
+<td><input type="submit" name="Submit" value="Login"></td>
+</tr>
+</table>
+</td>
+</form>
+</tr>
+</table>
+
+		  </div>
+		</div>
+		</body>
+		</html>
