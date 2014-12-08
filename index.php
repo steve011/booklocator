e267456ea75a967eef699b4851ca3a45f6b4240c
steve011 <stevenwasi@gmail.com>
2014-12-08 18:22:05 -0500

    Update index.php

--- index.php
+++ index.php
@@ -43,6 +43,16 @@ session_start();
   					{
   					$isbn = $row["ISBN"];
   					$_SESSION['product'] = $row;
+  					}
+  					foreach($_SESSION['product'] as $key)
+  					{
+  						 $stid = oci_parse($connection, 'SELECT * FROM (SELECT * FROM books ORDER BY DBMS_RANDOM.VALUE) WHERE ROWNUM <= 5'); /* Added "WHERE ROWNUM <= 1000", takes forever to load otherwise */
+					 oci_execute($stid);
+  					while($row = oci_fetch_array($stid))
+  					{ $stid = oci_parse($connection, 'SELECT * FROM (SELECT * FROM books ORDER BY DBMS_RANDOM.VALUE) WHERE ROWNUM <= 5'); /* Added "WHERE ROWNUM <= 1000", takes forever to load otherwise */
+					 oci_execute($stid);
+  					while($row = oci_fetch_array($stid))
+  					{
   					echo '<a href="product.php?Product='.htmlentities($row["ISBN"]).'">';
   					echo '<div class="col-xs-2" style="height:300px;margin:19.5px;;background-size:100% 100%;>';
   					echo '<img style="z-index:1;position:absolute;height:250px;width:100%;" src="http://i.imgur.com/pV1XQjk.jpg">';
@@ -50,9 +60,7 @@ session_start();
 					echo '<div class="width:100%;text-align:center;color:white;border-top:1px solid black;">';
 					echo '<p style="font-size:12px;text-align:center;">'.htmlentities($row["TITLE"]).'</p>';
 					echo '</div></div></a>';
-					}
-					foreach($_SESSION['product'] as $key=>$val){
-						header('Location: product.php');
+  					}
 					}
 					 ?>
 			</div>	
