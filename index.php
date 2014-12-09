23f1925832544dfba363c1b2e48ad38e49ad37bd
steve011 <stevenwasi@gmail.com>
2014-12-09 04:34:48 -0500

    Update index.php

--- index.php
+++ index.php
@@ -76,7 +76,7 @@ function get_average_rating($isbn, $conn){
 					echo '<img style="z-index:2;position:relative;height:250px;width:100%;" src="'.htmlentities($row["IMAGE_URL_L"]).'">';
 					echo '<div class="width:100%;text-align:center;color:white;border-top:1px solid black;">';
 					echo '<p style="font-size:12px;text-align:center;">'.htmlentities($row["TITLE"]).'</p>';
-					echo '<p style="font-size:12px;text-align:center;"> Average Rating: '.$avg_rating.'</p>';
+					echo '<p style="font-size:12px;text-align:center;"> Average Rating: '.htmlentities($avg_rating).'</p>';
 					if(isset($row["PRICE"])) echo '<p style="font-size:12px;text-align:center;">$'.htmlentities($row["PRICE"]).'</p>';
 					echo '</div></div></a>';
 					}
