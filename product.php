5821f3557ebbcf36bc90d4812d22988ac56b9fb3
steve011 <stevenwasi@gmail.com>
2014-12-09 09:38:55 -0500

    Update product.php

--- product.php
+++ product.php
@@ -77,7 +77,7 @@ session_start();
 				</div>
 				<div class="col-xs-12" style="margin-top:20px">
 					<?php
-					$stid = oci_parse($connection, 'SELECT * FROM BOOKS WHERE title LIKE '%$row['TITLE']%''); /* Added "WHERE ROWNUM <= 1000", takes forever to load otherwise */
+					$stid = oci_parse($connection, 'SELECT * FROM BOOKS WHERE genre = $row['GENRE']'); /* Added "WHERE ROWNUM <= 1000", takes forever to load otherwise */
 					 oci_execute($stid);
   					while($row = oci_fetch_array($stid))
   					{
