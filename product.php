747a2704aaf0b49df005cd567bf02c2ad55779eb
steve011 <stevenwasi@gmail.com>
2014-12-09 01:09:03 -0500

    Update product.php

--- product.php
+++ product.php
@@ -73,7 +73,7 @@ session_start();
 					<?php  
 						$iframe_url = $row['REVIEW_IFRAME_URL'];
 						$iframe_url = str_replace("exp=2014-12-08", "exp=2015-12-08", $iframe_url);
-						echo '<iframe src="'.$iframe_url.'" style="width:650px;height:600px;"></iframe>';
+						echo '<iframe src="'.$iframe_url.'" style="width:auto;height:600px;"></iframe>';
 					?>
 					<!--
 					</div>
