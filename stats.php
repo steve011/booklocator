f360da2e7d57ab26d5df5a59d78ed2325dfa8455
steve011 <stevenwasi@gmail.com>
2014-12-08 22:32:09 -0500

    Update stats.php

--- stats.php
+++ stats.php
@@ -8,8 +8,50 @@ session_start();
 <html><head><title>PHP Test</title></head>
 <body>
 
-<h2>Search</h2>
-
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="bootstrap.css">
+		<link rel="stylesheet" href="font-awesome-4.2.0/css/font-awesome.css">
+		<link rel="stylesheet" href="font-awesome-4.2.0/css/font-awesome.min.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	</head>
+	<body>	
+		<header>
+			<div class="container" style="height:150px;">
+				<div class="row" style="">
+					<div class="col-xs-8 col-xs-push-2" style="height:100%;background-color:#1ad2ff;z-index:2;border: 3px solid black;border-radius:10px;margin-top:-5px;">
+						<h1 style="font-family:cursive;font-weight:bold;text-align:center;font-style:italic;margin-top:25px;letter-spacing:2px;color:white">BOOK LOCATOR</h1>
+						<?php 
+ 						$username=$_SESSION['username'];
+						if($_SESSION['username'])
+						{
+						echo '<p style="position:relative;left:0px;bottom:-45px;">';
+						echo $_SESSION['username'];
+						echo '</p>';
+						echo '<a href="index.php" style="cursor:pointer;position:relative;left:95%;bottom:-15px;" onclick="';
+						unset($_SESSION['username']);
+						echo '">Logout</a>';
+						}
+						?>
+					</div>
+				</div>
+				<Div class="row">
+					<div class="col-xs-6 col-xs-push-3 menu-bar" style="background-color:#ff471a;z-index:1;">
+							<ul>
+								<li><a href="index.php">Home</a></li>
+								<li><a href="collections.php">Store<a></li>
+								<li><a href="http://cise.ufl.edu/~sw5/Book_Locator/admin.php">Search</a></li>
+								<li><a href="http://cise.ufl.edu/~sw5/Book_Locator/adminer.php?oracle=oracle.cise.ufl.edu%2Forcl&username=sw5&db=CISETS&ns=SW5">Support</a></li>
+								<li><a href="login.php">Login</a></li>
+							</ul>
+					</div>
+				</div>
+			</div>
+		</header>
+		<br></br>
+<div class="container">
+<div class="row">
+<div class="col-xs-4;"
 <?php
 	$stid = oci_parse($connection, 'SELECT location, COUNT(*) FROM USERS GROUP BY location ORDER BY COUNT(*) DESC'); /* Added "WHERE ROWNUM <= 1000", takes forever to load otherwise */
 	oci_execute($stid);
@@ -21,6 +63,7 @@ session_start();
   		echo '<br></br>';
 	}
 ?>
+</div></div></div>
 <?php
 oci_close($connection);
 ?>
