0c9f7958570265e7a7396d8e13a1334152405bf2
steve011 <stevenwasi@gmail.com>
2014-12-08 22:19:47 -0500

    Update stats.php

--- stats.php
+++ stats.php
@@ -10,42 +10,17 @@ session_start();
 
 <h2>Search</h2>
 
-<?php 
-$my_query = 'SELECT location,  COUNT(*) FROM USERS GROUP BY location;';
-
-		$stid_count = 0;
-		$query = $my_query;
-		$query .= " AND ROWNUM <= 1000";
-	
-		print '<table border="1">';
-	
-			$stid = oci_parse($connection, "$query");
-			print '<tr>';
-			echo "<p><b> Results found: "; 
-			oci_execute($stid);
-			echo "$count</b><p>";
-			print '</tr>';
-			
-			
-				$ncols = oci_num_fields($stid);
-				print '<tr>';
-				for ($i = 1; $i <= $ncols; $i++) {
-					$column_name  = oci_field_name($stid, $i);
-					echo "<td><b>$column_name</b></td>";
-				
-				print '</tr>';
-	
-			while ($row = oci_fetch_array($stid, OCI_RETURN_NULLS+OCI_ASSOC)) {
-				print '<tr>';
-				foreach ($row as $item) {
-						print '<td>'.($item !== null ? htmlentities($item, ENT_QUOTES) : '&nbsp').'</td>';
-				}
-				print '</tr>';
-			}
-		
-		print '</table>';
-		oci_free_statement($stid);
-
+<?php
+	 $stid = oci_parse($connection, 'SELECT location,  COUNT(*) FROM USERS GROUP BY location;'); /* Added "WHERE ROWNUM <= 1000", takes forever to load otherwise */
+	oci_execute($stid);
+	 $value = 0;
+	$array = array();
+  	while($row = oci_fetch_array($stid))
+  	{
+  		echo $row;
+	}
+?>
+<?php
 oci_close($connection);
 ?>
 
