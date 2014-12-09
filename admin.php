f627a476479037c703c6216ced48b7cb0f3065ac
steve011 <stevenwasi@gmail.com>
2014-12-09 08:15:02 -0500

    Update admin.php

--- admin.php
+++ admin.php
@@ -7,8 +7,8 @@ $username=$_SESSION['username'];
 echo $username;
 //echo is_admin($username, $connection);
 //echo isset($_SESSION['username']);
-if(!isset($_SESSION['username']) || !is_admin($username, $connection)){
-	header('Location: login.php?msg=2');
+if(!is_admin($username, $connection)){
+	//header('Location: login.php?msg=2');
 }
 ?>
 
