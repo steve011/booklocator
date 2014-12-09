9e1614540b52846b4bafe46554691e3189250973
steve011 <stevenwasi@gmail.com>
2014-12-09 06:21:07 -0500

    Update index.php

--- index.php
+++ index.php
@@ -97,6 +97,12 @@ function get_average_rating($isbn, $conn){
 					 oci_execute($stid);
   					while($row = oci_fetch_array($stid))
   					{
+  					
+  					$isbn = $row["ISBN"];
+  					$_SESSION["$isbn"] = $row;
+  					$rating = get_average_rating($row["ISBN"], $connection);
+  					if($rating['AVERAGE_RATING'] > 0) $_SESSION["$isbn"]['RATING'] = $rating;
+  						
   					echo '<a href="product.php?Product='.htmlentities($row["ISBN"]).'">';
   					echo '<div class="col-xs-2" style="height:300px;margin:19.5px;background-image:url("");background-size:100% 100%;>';
   					echo '<img style="z-index:1;position:absolute;height:250px;width:100%;" src="http://i.imgur.com/pV1XQjk.jpg">';
@@ -117,6 +123,12 @@ function get_average_rating($isbn, $conn){
 					 oci_execute($stid);
   					while($row = oci_fetch_array($stid))
   					{
+  						
+  					$isbn = $row["ISBN"];
+  					$_SESSION["$isbn"] = $row;
+  					$rating = get_average_rating($row["ISBN"], $connection);
+  					if($rating['AVERAGE_RATING'] > 0) $_SESSION["$isbn"]['RATING'] = $rating;
+  						
   					echo '<a href="product.php?Product='.htmlentities($row["ISBN"]).'">';
   					echo '<div class="col-xs-2" style="height:300px;margin:19.5px;background-image:url("");background-size:100% 100%;>';
   					echo '<img style="z-index:1;position:absolute;height:250px;width:100%;" src="http://i.imgur.com/pV1XQjk.jpg">';
