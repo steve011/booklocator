7d0cdb6cb563d44f10356dc1803d08b35e47ebc8
steve011 <stevenwasi@gmail.com>
2014-12-09 04:44:19 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,12 @@ require ('../connect.php');
 session_start();
 function get_average_rating($isbn, $conn){
 	$avg_stid = oci_parse($conn, "Select sum(book_rating)/count(*) from ratings where isbn ='".$isbn."'");
+	$num_stid = oci_parse($conn, "Select count(*) from ratings where isbn ='".$isbn."'");
   	if(oci_execute($avg_stid)){
-  		return round(current(oci_fetch_array($avg_stid)), 2);
+  		$result['AVERAGE_RATING'] = round(current(oci_fetch_array($avg_stid)), 2);
+  		if (oci_execute($num_stid)){
+  			$result['NUMBER_OF_RATINGS'] = current(oci_fetch_array($num_stid));
+  		}
   	}else{
   		return -1;
   	}
