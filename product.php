fefed9b372757594e7acd4916ca44dcc7d26a760
steve011 <stevenwasi@gmail.com>
2014-12-08 23:15:30 -0500

    Update product.php

--- product.php
+++ product.php
@@ -36,11 +36,11 @@ session_start();
 		<div class="container">
 			<div class="row" style="">
 			<?php
-			echo '<h1>', $_GET['Product'], '</h1>';
+			//echo '<h1>', $_GET['Product'], '</h1>';
 			
 			$row = $_SESSION['Product'];
 				echo '<div class="col-xs-4" style="height:440px;padding-top:20px;float:left;">';
-					echo '<img src="' .htmlentities($row["IMAGE_URL_L"]). '"style="height:400px;width:100%;">';
+					echo '<img src="' .htmlentities($_SESSION["IMAGE_URL_L"]). '"style="height:400px;width:100%;">';
 			?>
 				</div>
 				<div class="col-xs-8" style="height:440px;background-color:red;padding-top:20px;float:right;">
